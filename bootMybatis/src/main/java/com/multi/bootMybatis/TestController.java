4c1a96f4263600eaaa0ddbaed0db21206f0b019e
kim moo young <fuvg13.gmail.com>
2021-04-26 16:10:16 +0900

    UpLoad

---
+++ bootMybatis/src/main/java/com/multi/bootMybatis/TestController.java
@@ -0,0 +1,21 @@
+package com.multi.bootMybatis;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	/*
+	 * @ResponseBody
+	 * 
+	 * @RequestMapping("/") // 요청 URL : http://localhost:8080 public String test() {
+	 * return "boot MyBatis"; }
+	 */
+	
+	// index 페이지 열기
+	/*
+	 * @RequestMapping("/") // 요청 URL : http://localhost:8080 public String
+	 * viewIndex() { return "index"; // index.jsp }
+	 */
+}
